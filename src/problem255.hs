14a816da1293d9ed74ce0fb588e702acf9f5840b
Daniel Molina Wegener <dmw@coder.cl>
2012-04-16 19:20:37 -0300

    problem 255 added

---
+++ src/problem255.hs
@@ -0,0 +1,21 @@
+module Main (main) where
+
+import Data.Digits (digits)
+import System.Environment (getArgs)
+
+digitBase :: Int -> Int
+digitBase n | odd n = round ((2 * 10) ** ((fromIntegral n - 1) / 2))
+digitBase n = round ((7 * 10) ** ((fromIntegral n - 2) / 2))
+
+heronOp :: Int -> Int -> Int -> Int
+heronOp o n m | n == m = m
+heronOp o n m = heronOp o m (round ((x + y) / 2))
+                where x = fromIntegral m
+                      y = fromIntegral o / x
+
+main :: IO ()
+main = do [x] <- getArgs
+          print
+            $ heronOp (read x) (read x)
+            $ digitBase $ length $ digits (read x) 10
+
