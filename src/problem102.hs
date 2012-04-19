fed8c0c289f6954afdb463cc3fc34c118ece0184
Daniel Molina Wegener <dmw@coder.cl>
2012-04-19 07:48:28 -0300

    *. Enhanced Problem 102 visualization.

--- src/problem102.hs
+++ src/problem102.hs
@@ -6,9 +6,11 @@ module Main (main) where
 import Data.List.Split
 import Data.String.Utils (strip)
 import Graphics.Rendering.OpenGL
+import qualified Graphics.Rendering.FTGL as F
 import Graphics.UI.GLUT
 import System.Environment
-import System.IO
+import System.Exit
+import Text.Printf
 
 
 data LPt = LPt (Int, Int)
@@ -40,24 +42,39 @@ instance Num LPt where
 crossPt :: LPt -> LPt -> LPt
 crossPt x y = LPt (0, fstp x * sndp y - sndp x * fstp y)
 
+
 zidxZero :: GLfloat
 zidxZero = 0.0 :: GLfloat
+
+
+colorCyan :: IO ()
+colorCyan = color $ Color3 0 (1.0 :: GLfloat) (1.0 :: GLfloat)
+
+
+colorGreen :: IO ()
+colorGreen = color $ Color3 0 (1.0 :: GLfloat) 0
+
 
 zeroPt :: LPt
 zeroPt = LPt (0, 0)
 
+
 fstp :: LPt -> Int
 fstp (LPt (x, y)) = x
 
+
 sndp :: LPt -> Int
 sndp (LPt (x, y)) = y
+
 
 ptGlF :: LPt -> (GLfloat, GLfloat)
 ptGlF p = ((fromIntegral (fstp p) / 1000.0) :: GLfloat,
            (fromIntegral (sndp p) / 1000.0) :: GLfloat)
 
+
 ptToGlPt :: LPt -> IO ()
 ptToGlPt p = vertex $ uncurry Vertex3 (ptGlF p) zidxZero
+
 
 mkTriangle :: String -> LTrg
 mkTriangle s = LTrg { tA = head g, tB = g !! 1, tC = last g }
@@ -65,20 +82,24 @@ mkTriangle s = LTrg { tA = head g, tB = g !! 1, tC = last g }
                          $ splitEvery 2 ln
                      ln = fmap (read . strip) $ splitOn "," s
 
+
 trgColor :: LTrg -> IO ()
-trgColor xs = if trgContained zeroPt xs
-                 then color $ Color3 0 0 (1.0 :: GLfloat)
-              else color $ Color3 0 (0.25 :: GLfloat) 0
+trgColor xs | trgContained zeroPt xs = colorCyan
+trgColor xs = colorGreen
 
-displayTriangle :: LTrg -> IO ()
-displayTriangle xs = do renderPrimitive Polygon $ do
-                          trgColor xs
-                          mapM ptToGlPt [tA xs, tB xs, tC xs]
-                        flush
 
-displayTriangles :: [LTrg] -> IO ()
-displayTriangles xs = do clear [ ColorBuffer, DepthBuffer ]
-                         mapM_ displayTriangle xs
+displayText :: String -> F.Font -> IO ()
+displayText text font = do
+  F.renderFont font text F.All
+  flush
+
+
+trgTextCont :: [LTrg] -> F.Font -> IO ()
+trgTextCont xs = displayText text
+                 where text = printf "Match: %d | Unmatch: %d" cnts cnts2
+                       cnts2 = length xs - cnts
+                       cnts = length $ filter (trgContained zeroPt) xs
+
 
 mkTriangles :: String -> Int -> Int -> [LTrg]
 mkTriangles s l h = drop l
@@ -86,6 +107,7 @@ mkTriangles s l h = drop l
                     $ fmap mkTriangle
                     $ filter (\ x -> length x > 0 )
                     $ lines s
+
 
 trgContained :: LPt -> LTrg -> Bool
 trgContained p t = sndp crs1 >= 0 && sndp crs2 >= 0 && sndp crs3 >= 0
@@ -97,16 +119,43 @@ trgContained p t = sndp crs1 >= 0 && sndp crs2 >= 0 && sndp crs3 >= 0
                          seg2 = tC t - tB t
                          seg3 = tA t - tC t
 
-main :: IO ()
-main = do [x,y,z] <- getArgs
-          inp <- readFile x
-          (progname, _) <- getArgsAndInitialize
-          loadIdentity
-          initialWindowSize $= Size 800 800
-          createWindow "Trimatrix Display"
-          matrixMode $= Projection
-          polygonMode $= (Line, Line)
-          displayCallback $= displayTriangles (mkTriangles inp (read y) (read z))
-          mainLoop
 
+displayTriangle :: LTrg -> IO ()
+displayTriangle xs = do _ <- renderPrimitive Polygon $ do
+                          trgColor xs
+                          mapM ptToGlPt [tA xs, tB xs, tC xs]
+                        flush
+
+
+displayTriangles :: [LTrg] -> F.Font -> IO ()
+displayTriangles xs f = do clear [ ColorBuffer, DepthBuffer ]
+                           mapM_ displayTriangle xs
+                           trgTextCont xs f
+                           flush
+
+
+handlerIOError :: IOError -> IO ()
+handlerIOError e = putStrLn (printf "IOError: %s" $ show e)
+                   >> putStrLn (printf "Usage: problem102 triangles.txt low-bound hi-bound")
+                   >> exitFailure
+
+
+mainGl :: IO ()
+mainGl = do [x,y,z] <- getArgs
+            inp <- readFile x
+            (progname, _) <- getArgsAndInitialize
+            loadIdentity
+            initialWindowSize $= Size 800 800
+            _ <- createWindow "Triangles Display"
+            matrixMode $= Projection
+            polygonMode $= (Line, Line)
+            font <- F.createPixmapFont "aller.ttf"
+            _ <- F.setFontFaceSize font 24 24
+            _ <- F.setFontDepth font 1.0
+            displayCallback $= displayTriangles (mkTriangles inp (read y) (read z)) font
+            mainLoop
+
+
+main :: IO ()
+main = mainGl `catch` handlerIOError
 
